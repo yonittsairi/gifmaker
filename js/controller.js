04f57cfeb42e7a51794ee5d3dd815f2688429da3
yonittsairi <yonittsairi@gmail.com>
2020-12-12 21:22:25 +0200

    v7

--- js/controller.js
+++ js/controller.js
@@ -147,15 +147,7 @@ function mouseUp(ev) {
     }
 }
 
-// function drawLine(context, x1, y1, x2, y2) {
-//     context.beginPath();
-//     context.strokeStyle = 'black';
-//     context.lineWidth = 1;
-//     context.moveTo(x1, y1);
-//     context.lineTo(x2, y2);
-//     context.stroke();
-//     context.closePath();
-// }
+
 
 function getLine(ev) {
     var { offsetX, offsetY } = ev;
@@ -424,3 +416,35 @@ function closeMenu() {
 }
 
 // scale ctx.scale(2, 2);
+
+function uploadImg(elForm, ev) {
+    ev.preventDefault();
+    document.getElementById('imgData').value = gCanvas.toDataURL("image/jpeg");
+
+    // A function to be called if request succeeds
+    function onSuccess(uploadedImgUrl) {
+        uploadedImgUrl = encodeURIComponent(uploadedImgUrl)
+        document.querySelector('.share-container').innerHTML = `
+        <a class="btn facbook" href="https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}" title="Share on Facebook" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}'); return false;">
+           Share   
+        </a>`
+
+    }
+
+    doUploadImg(elForm, onSuccess);
+}
+
+function doUploadImg(elForm, onSuccess) {
+    var formData = new FormData(elForm);
+    fetch('http://ca-upload.com/here/upload.php', {
+        method: 'POST',
+        body: formData
+    })
+        .then(function (res) {
+            return res.text()
+        })
+        .then(onSuccess)
+        .catch(function (err) {
+            console.error(err)
+        })
+}
